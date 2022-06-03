7b78197b1a432e60bc4d0c5b9ec2bd4ecda83634
idk <hankhill19580@gmail.com>
2022-06-03 19:17:14 -0400

    switch from gothub to github-release

--- windows-nightly.sh
+++ windows-nightly.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 cd "$(dirname "$0")"
 git pull --all
-CGO_ENABLED=0 GOOS=windows go build $(WIN_GO_COMPILER_OPTS) -o $(packagename)-windows.exe
+CGO_ENABLED=0 GOOS=windows go build -a -tags "netgo osusergo windows" -ldflags '-H=windowsgui' -o brb-windows.exe
 make daily
 sleep 2s
 make upload-windows-daily
