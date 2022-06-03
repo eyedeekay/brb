95785c3addf3213edcaddd296abd63b8fdeab6e0
idk <hankhill19580@gmail.com>
2022-06-03 19:16:14 -0400

    switch from gothub to github-release

--- windows-nightly.sh
+++ windows-nightly.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 cd "$(dirname "$0")"
 git pull --all
-make windows-runner
+CGO_ENABLED=0 GOOS=windows go build $(WIN_GO_COMPILER_OPTS) -o $(packagename)-windows.exe
 make daily
 sleep 2s
 make upload-windows-daily
